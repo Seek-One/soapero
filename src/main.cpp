102be09ae70b9b6e16402c2a4cfc61e79cdef2dc
Lucas Gruber <lucas.gruber@jet1oeil.com>
2017-07-12 15:25:59 +0000

    initial project

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 12 juil. 2017
+ *      Author: lgruber
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#ifdef HAVE_VERSION_H
+#include <version.h>
+#endif
+
+#include <QCoreApplication>
+
+int main(int argc, char **argv)
+{
+	qDebug("Jet1oeil Soapero");
+	return 0;
+}
