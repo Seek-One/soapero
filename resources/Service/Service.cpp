a5d5d6e46c65aeda40f47c53fcd160c70de6e356
Eric Beuque <eric.beuque@jet1oeil.com>
2022-06-14 12:06:38 +0200

    Fix compilation

--- resources/Service/Service.cpp
+++ resources/Service/Service.cpp
@@ -85,32 +85,32 @@ public:
 	}
 };
 
-static int p_gettimeofday(struct timeval* p, void* tz)
-{
 #ifdef _MSC_VER
-	ULARGE_INTEGER ul; // As specified on MSDN.
-	FILETIME ft;
+	static int p_gettimeofday(struct timeval* p, void* tz)
+	{
+		ULARGE_INTEGER ul; // As specified on MSDN.
+		FILETIME ft;
 
-	// Returns a 64-bit value representing the number of
-	// 100-nanosecond intervals since January 1, 1601 (UTC).
-	GetSystemTimeAsFileTime(&ft);
+		// Returns a 64-bit value representing the number of
+		// 100-nanosecond intervals since January 1, 1601 (UTC).
+		GetSystemTimeAsFileTime(&ft);
 
-	// Fill ULARGE_INTEGER low and high parts.
-	ul.LowPart = ft.dwLowDateTime;
-	ul.HighPart = ft.dwHighDateTime;
-	// Convert to microseconds.
-	ul.QuadPart /= 10ULL;
-	// Remove Windows to UNIX Epoch delta.
-	ul.QuadPart -= 11644473600000000ULL;
-	// Modulo to retrieve the microseconds.
-	p->tv_usec = (long) (ul.QuadPart % 1000000LL);
-	// Divide to retrieve the seconds.
-	p->tv_sec = (long) (ul.QuadPart / 1000000LL);
-	return 0;
+		// Fill ULARGE_INTEGER low and high parts.
+		ul.LowPart = ft.dwLowDateTime;
+		ul.HighPart = ft.dwHighDateTime;
+		// Convert to microseconds.
+		ul.QuadPart /= 10ULL;
+		// Remove Windows to UNIX Epoch delta.
+		ul.QuadPart -= 11644473600000000ULL;
+		// Modulo to retrieve the microseconds.
+		p->tv_usec = (long) (ul.QuadPart % 1000000LL);
+		// Divide to retrieve the seconds.
+		p->tv_sec = (long) (ul.QuadPart / 1000000LL);
+		return 0;
+	}
 #else
-	return gettimeofday(p, tz);
+#define p_gettimeofday gettimeofday
 #endif
-}
 
 Service::Service()
 {
